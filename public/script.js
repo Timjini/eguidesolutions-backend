525dc1114c1496daadbe0709259a06334a011630
Timjini <hatim.jini@gmail.com>
2023-08-16 10:58:14 +0300

    node js server

---
+++ public/script.js
@@ -0,0 +1,74 @@
+const socket = io('/');
+
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+});
+
+// Define the connectToNewUser function
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream);
+    const audio = document.createElement('audio');
+    
+    call.on('stream', userAudioStream => {
+        audio.srcObject = userAudioStream;
+        audio.play();
+    });
+
+    call.on('close', () => {
+        audio.remove();
+    });
+
+    peers[userId] = call;
+}
+
+// Initialize the peers object
+const peers = {}; // Create an empty object to store the WebRTC connections
+
+// Rest of your code
+const myAudio = document.createElement('audio');
+myAudio.muted = true;
+
+let isMuted = false; // Track mute state
+
+navigator.mediaDevices.getUserMedia({
+    video: false,
+    audio: true
+}).then(stream => {
+    myAudio.srcObject = stream;
+
+    myPeer.on('call', call => {
+        call.answer(stream);
+        const audio = document.createElement('audio');
+        call.on('stream', userAudioStream => {
+            audio.srcObject = userAudioStream;
+            audio.play();
+        });
+    });
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream);
+    });
+
+    // Toggle mute state when the mute button is clicked
+    const muteButton = document.getElementById('muteButton');
+    muteButton.addEventListener('click', () => {
+        myAudio.muted = true;
+        console.log('Mute state: ' + isMuted);
+    });
+});
+
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close();
+});
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+});
+
+socket.on('user-connected', userId => {
+    console.log('user connected: ' + userId);
+});
+
+
+
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const cors = require('cors');
+const { v4: uuidV4 } = require('uuid');
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        console.log(roomId, userId);
+       socket.join(roomId)
+       socket.broadcast.to(roomId).emit('user-connected', userId);
+    //    socket.to(roomId).broadcast.emit('user-connected', userId)
+    });
+});
+
+
+
+server.listen(4000)
