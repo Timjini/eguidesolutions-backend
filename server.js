e7b565cf9f0c3ba2830ebf1604be7182ca840202
Timjini <hatim.jini@gmail.com>
2023-08-16 13:38:22 +0300

    require path

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const { PeerServer } = require('peer'); // Import the PeerServer
 
 app.use(cors());
 
+const path = require('path'); // Add this line
+
+app.set('views', path.join(__dirname, 'views')); // Update this line if necessary
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
