04d7ea8d4920199fc866117deb9d3fef0fe66076
Timjini <hatim.jini@gmail.com>
2023-08-17 07:35:27 +0300

    users model

---
+++ auth/authenticationMiddleware.js
@@ -0,0 +1,32 @@
+// authenticationMiddleware.js
+
+const jwt = require('jsonwebtoken');
+const User = require('../models/Users'); // Adjust the path accordingly
+
+const authenticationMiddleware = (socket, next) => {
+  const authToken = socket.handshake.query.authToken;
+
+  if (!authToken) {
+    return next(new Error('Authentication failed. No authToken provided.'));
+  }
+
+  try {
+    const decoded = jwt.verify(authToken, 'secretKey');
+    console.log(secretKey + "secretKey")
+    const userId = decoded.userId;
+
+    User.findOne({ userId }).then(user => {
+      if (!user) {
+        return next(new Error('Authentication failed. User not found.'));
+      }
+
+      // Attach user object to socket for further use
+      socket.user = user;
+      next();
+    });
+  } catch (error) {
+    return next(new Error('Authentication failed. Invalid authToken.'));
+  }
+};
+
+module.exports = authenticationMiddleware;
