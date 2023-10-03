281e4541da938788d734d3f0edc81471878fcc43
Timjini <hatim.jini@gmail.com>
2023-10-03 15:16:51 +0300

    test script.js

--- public/script.js
+++ public/script.js
@@ -1,64 +1,66 @@
-console.log('peer');
-const ROOM_ID = window.location.pathname.replace('/', ''); // Extract the room ID from the URL
-const socket = io('/');
+// console.log('peer');
+// const ROOM_ID = window.location.pathname.replace('/', ''); // Extract the room ID from the URL
+// const socket = io('/');
 
-const myPeer = new Peer(undefined, {
-    host: window.location.hostname,
-    port: 5000,
-    path: '/'
-});
+// const myPeer = new Peer(undefined, {
+//     host: window.location.hostname,
+//     port: 5000,
+//     path: '/'
+// });
 
-const peers = {}; // Store all connected peers
+// const peers = {}; // Store all connected peers
 
-navigator.mediaDevices.getUserMedia({
-    video: false,
-    audio: true
-}).then(stream => {
-    const myAudio = document.createElement('audio');
-    myAudio.srcObject = stream;
-    myAudio.play();
+// navigator.mediaDevices.getUserMedia({
+//     video: false,
+//     audio: true
+// }).then(stream => {
+//     const myAudio = document.createElement('audio');
+//     myAudio.srcObject = stream;
+//     myAudio.play();
 
-    myPeer.on('call', call => {
-        call.answer(stream);
-        const peerAudio = document.createElement('audio');
+//     myPeer.on('call', call => {
+//         call.answer(stream);
+//         const peerAudio = document.createElement('audio');
 
-        call.on('stream', userAudioStream => {
-            peerAudio.srcObject = userAudioStream;
-            peerAudio.play();
-        });
+//         call.on('stream', userAudioStream => {
+//             peerAudio.srcObject = userAudioStream;
+//             peerAudio.play();
+//         });
 
-        call.on('close', () => {
-            peerAudio.remove();
-        });
-    });
+//         call.on('close', () => {
+//             peerAudio.remove();
+//         });
+//     });
 
-    socket.on('user-connected', () => {
-        const call = myPeer.call(undefined, stream); // Call without specifying a user ID
-        const peerAudio = document.createElement('audio');
+//     socket.on('user-connected', () => {
+//         const call = myPeer.call(undefined, stream); // Call without specifying a user ID
+//         const peerAudio = document.createElement('audio');
 
-        call.on('stream', userAudioStream => {
-            peerAudio.srcObject = userAudioStream;
-            peerAudio.play();
-        });
+//         call.on('stream', userAudioStream => {
+//             peerAudio.srcObject = userAudioStream;
+//             peerAudio.play();
+//         });
 
-        call.on('close', () => {
-            peerAudio.remove();
-        });
-    });
+//         call.on('close', () => {
+//             peerAudio.remove();
+//         });
+//     });
 
-    socket.on('user-disconnected', () => {
-        const call = myPeer.call(undefined, stream);
+//     socket.on('user-disconnected', () => {
+//         const call = myPeer.call(undefined, stream);
 
-    });
-});
-
-
-const muteButton = document.getElementById('muteButton');
-muteButton.addEventListener('click', () => {
-    // Add logic to mute/unmute audio
-});
+//     });
+// });
 
 
-myPeer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id);
-});
+// const muteButton = document.getElementById('muteButton');
+// muteButton.addEventListener('click', () => {
+//     // Add logic to mute/unmute audio
+// });
+
+
+// myPeer.on('open', id => {
+//     socket.emit('join-room', ROOM_ID, id);
+// });
+
+console.log("script.js")
