d1909db5502635afc50d4a0789bf677f7f228fc2
Timjini <hatim.jini@gmail.com>
2023-10-03 15:14:53 +0300

    update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 console.log('peer');
-const ROOM_ID = window.location.pathname.replace('/', '');
+const ROOM_ID = window.location.pathname.replace('/', ''); // Extract the room ID from the URL
 const socket = io('/');
 
 const myPeer = new Peer(undefined, {
@@ -14,58 +14,51 @@ navigator.mediaDevices.getUserMedia({
     video: false,
     audio: true
 }).then(stream => {
-    // Set up your own audio stream
     const myAudio = document.createElement('audio');
     myAudio.srcObject = stream;
     myAudio.play();
 
-    // When you receive a connection from a new user
     myPeer.on('call', call => {
-        call.answer(stream); // Answer the call with your stream
+        call.answer(stream);
         const peerAudio = document.createElement('audio');
 
-        // When the remote user's stream is available, play it
         call.on('stream', userAudioStream => {
             peerAudio.srcObject = userAudioStream;
             peerAudio.play();
         });
 
-        // Handle the call being closed
         call.on('close', () => {
             peerAudio.remove();
         });
     });
 
-    // When a new user connects, call them and send your stream
-    socket.on('user-connected', userId => {
-        const call = myPeer.call(userId, stream);
+    socket.on('user-connected', () => {
+        const call = myPeer.call(undefined, stream); // Call without specifying a user ID
         const peerAudio = document.createElement('audio');
 
-        // When the remote user's stream is available, play it
         call.on('stream', userAudioStream => {
             peerAudio.srcObject = userAudioStream;
             peerAudio.play();
         });
 
-        // Handle the call being closed
         call.on('close', () => {
             peerAudio.remove();
         });
-
-        // Store the call object in your peers list
-        peers[userId] = call;
     });
 
-    // When a user disconnects, close the call and remove their audio element
-    socket.on('user-disconnected', userId => {
-        if (peers[userId]) {
-            peers[userId].close();
-            delete peers[userId];
-        }
+    socket.on('user-disconnected', () => {
+        const call = myPeer.call(undefined, stream);
+
     });
 });
 
+
+const muteButton = document.getElementById('muteButton');
+muteButton.addEventListener('click', () => {
+    // Add logic to mute/unmute audio
+});
+
+
 myPeer.on('open', id => {
-    // Emit the room ID and peer ID to the server for tracking
     socket.emit('join-room', ROOM_ID, id);
 });
