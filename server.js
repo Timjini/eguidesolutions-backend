4060d83ed8138d9ee9ff62470f590c58670356a8
Timjini <hatim.jini@gmail.com>
2023-10-03 15:09:40 +0300

    go back

--- server.js
+++ server.js
@@ -37,8 +37,7 @@ app.use(cors({
   origin: allowedOrigins
 }));
 
-// app.set('views', path.join(__dirname, 'views')); // Update this line if necessary
-app.use(express.static(path.join(__dirname, "js")));
+app.set('views', path.join(__dirname, 'views')); // Update this line if necessary
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
