5e285ac7be5fdab19a9b5a507102fdd890528156
Timjini <hatim.jini@gmail.com>
2023-10-03 15:06:15 +0300

    update

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
+
 // app.use(verifyToken); 
 app.use('/api/users', usersRoutes);
 app.use('/api/channels', channelsRoutes);
