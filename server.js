698658ac2595fa876a4c9d436502773aa37db97a
Timjini <hatim.jini@gmail.com>
2023-10-09 14:45:30 +0300

    audio testing

--- server.js
+++ server.js
@@ -1,120 +1,32 @@
 const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const cors = require('cors');
-const { v4: uuidV4 } = require('uuid');
-const { PeerServer } = require('peer'); // Import the PeerServer
-const crypto = require('crypto');
-const verifyToken = require("./auth/authMiddleware");
-const Room = require('./models/Channels');
-const multer = require('multer');
-const User = require('./models/Users'); 
-const usersRoutes = require('./api/users/usersRoutes');
-const channelsRoutes = require('./api/channels/channelsRoutes');
-const agenciesRoutes = require('./api/agencies/agenciesRoutes');
-const toursRoutes = require('./api/tours/toursRoutes');
 const http = require('http');
-const socketIo = require('socket.io');
+const WebSocket = require('ws');
 
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-const path = require('path'); // Add this line
-const { error } = require('console');
+// Serve static files from the public directory
+app.use(express.static('views'));
 
-const storage = multer.diskStorage({
-  destination: path.join(__dirname, './public/uploads'),
-  filename: (req, file, callback) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const extension = path.extname(file.originalname);
-    callback(null, uniqueSuffix + extension);
-  },
-});
+wss.on('connection', (ws) => {
+  console.log('Client connected');
 
-  
-const upload = multer({ storage });
-  
-const allowedOrigins = ['https://admin-eguide.vercel.app', 'https://admin.e-guidesolutions.com', 'http://localhost:5173', 'http://localhost:3000'];
+  ws.on('message', (message) => {
+    // Broadcast received audio data to all connected clients except the sender
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
 
-
-app.use(cors({
-  origin: allowedOrigins
-}));
-
-app.set('views', path.join(__dirname, 'views')); // Update this line if necessary
-// app.use('/public', express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
-// app.use(express.static('public'));
-app.use(express.json());
-
-// app.use(express.static('public')); 
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-// app.use(express.json());
-
-// app.use(verifyToken); 
-app.use('/api/users', usersRoutes);
-app.use('/api/channels', channelsRoutes);
-app.use('/api/tours', toursRoutes);
-app.use('/api/agencies', agenciesRoutes);
-
-app.post("/welcome", verifyToken, (req, res) => {
-  res.json({ message: 'You have access to this secure route!', user: req.user });
-});
-
-
-
-
-// io.use(authenticationMiddleware);
-// io.on('connection', async socket => {
-//     const authToken = socket.handshake.query.authToken;
-  
-//     try {
-//       const user = await User.findOne({ authToken }); 
-  
-//       if (!user) {
-//         return; 
-//       }
-  
-//       // You can now access the user's properties, including user.email
-//       console.log('User connected:', user.email);
-  
-//       socket.on('join-room', (roomId, userId) => {
-//         console.log('Room joined:', roomId);
-//         socket.join(roomId);
-//         socket.broadcast.to(roomId).emit('user-connected', userId);
-//       });
-  
-//       // Other socket event handlers here
-  
-//     } catch (error) {
-//       // Handle any potential errors
-//       console.error('Error while handling socket connection:', error);
-//     }
-//   });
-
-  
-app.get('/' , (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-});
-
-app.get('/:room' , (req, res) => {
-  res.render('room', {roomId: req.params.room});
-});
-
-
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-      socket.join(roomId);
-      socket.to(roomId).broadcast.emit('user-connected', userId); // Broadcast the user connection to others in the room
-
-      socket.on('disconnect', () => {
-          socket.to(roomId).broadcast.emit('user-disconnected', userId); // Broadcast the user disconnection to others in the room
-      });
+  ws.on('close', () => {
+    console.log('Client disconnected');
   });
 });
 
-// const peerServer = PeerServer({ port: 5000, path: '/' });
-
-
-server.listen(4000)
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
