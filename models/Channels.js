86d2d0d1a61350850dfcb76b6b27c2f757ecb57f
Smartup Network <smartupnetwork@Smartup-MacBook-Pro.local>
2024-01-20 18:31:56 +0300

    remove letters from code only numbers

--- models/Channels.js
+++ models/Channels.js
@@ -34,7 +34,7 @@ function generateChannelId() {
 
 
 function generateChannelCode() {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = '0123456789';
   let code = '';
   for (let i = 0; i < 6; i++) {
     code += characters.charAt(Math.floor(Math.random() * characters.length));
