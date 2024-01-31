bca611ced0642bf75927d2049312a53ef028ce32
Smartup Network <smartupnetwork@192.168.1.13>
2024-01-31 14:24:20 +0300

    cors

--- server.js
+++ server.js
@@ -57,12 +57,6 @@ app.post('/upload', async (req, res) => {
 
 app.use(upload);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://admin.e-guidesolutions.com/');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 app.set('views', path.join(__dirname, 'views')); 
 app.use(express.static(path.join(__dirname, 'public')));
