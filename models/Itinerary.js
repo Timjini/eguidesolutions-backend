c052a3b20951e1ae3bf5c0a1cca62b0b8488bab4
Necmi Gunduz <necmigunduz@gmail.com>
2024-05-27 18:52:55 +0300

    reorganize: stops schema

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -1,11 +1,18 @@
 const mongoose = require('../db');
 
+const stopSchema = mongoose.Schema({
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+});
+
 const itinerarySchema = mongoose.Schema({
     tour: { type: mongoose.Schema.Types.ObjectId, ref: 'Tour' },
     agency: { type: mongoose.Schema.Types.ObjectId, ref: 'Agency' },
     start_point: { type: Object, required: true },
     end_point: { type: Object, required: true },
-    stops: { type: Array }
+    stops: { type: [stopSchema] },
+    createdAt: { type: Date, required: true, default: Date.now },
+    updatedAt: { type: Date, required: true, default: Date.now },
 });
 
 const Itinerary = mongoose.Model('Itinerary', itinerarySchema);
