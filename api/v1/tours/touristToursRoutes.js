6ec42d6f86421215b78f5fe616343e0ea55f6d27
Necmi Gunduz <necmigunduz@gmail.com>
2024-06-04 13:59:09 +0300

    move: createNewTour to tourController.js and touristTourRoutes

--- api/v1/tours/touristToursRoutes.js
+++ api/v1/tours/touristToursRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const TourController = require("../../../controllers/tourController");
 
 router.post("/", TourController.getAllTours);
+router.post("/new-route", TourController.createNewTour);
 
 module.exports = router;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,8 @@
 const Tour = require("../models/Tours");
 const Favorite = require("../models/Favorite");
 const TourSerializer = require("../serializers/tourSerializer");
+const Agency = require('../../models/Agency');
+const { upload, uploadToS3 } = require('../../fileUploader');
 
 class TourController {
   static async getAllTours(req, res) {
@@ -37,6 +39,45 @@ class TourController {
         status: "error",
         message: err.message,
       });
+    }
+  }
+
+  static async createNewTour(req, res) {
+    try {
+      const { title, description, guide, agency, startingDate, endingDate } = req.body;
+      const file = req.file;
+      const agencyId = req.body.agency
+      const image = await uploadToS3(file);
+
+      // const authToken = req.headers.authorization?.split(' ')[1];
+
+      const tour = new Tour({
+        title,
+        description,
+        photo: image.file_name,
+        guide: guide,
+        agency: agency,
+        starting_date: startingDate,
+        ending_date: endingDate
+      });
+
+      // Save the tour document to the database
+      await tour.save();
+
+      const agencyTour = await Agency.findOne({ _id: agencyId });
+
+      if (!agencyTour) {
+        return res.status(404).json({ error: 'Agency not found' });
+      }
+
+      // Update the user's profile to include this new tour (assuming you have a user-tour relationship)
+      agencyTour.tours.push(tour);
+      await agencyTour.save();
+
+      res.status(201).json({ message: 'Tour created successfully', tour });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while creating the tour' });
     }
   }
 }
