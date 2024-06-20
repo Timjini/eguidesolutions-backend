e4cb73df1a23bb7d797b612ec54be433442d06a6
Necmi Gunduz <necmigunduz@gmail.com>
2024-06-20 10:51:00 +0300

    add: createItinerary and createAddress to tour helper

--- helpers/TourHelper.js
+++ helpers/TourHelper.js
@@ -1,12 +1,38 @@
-function createItinerary(data, tour) {
+const Itinerary = require('../models/Itinerary');
+const Address = require('../models/Address');
+
+export const createAddress = (data) => {
+  const address = new Address({
+    street_1: data.street_1,
+    street_2: data.street_2,
+    city: data.city,
+    state: data.state,
+    country: data.country,
+    postal_code: data.postal_code,
+    coordinates: {
+      lang: data.lang,
+      lat: data.lat,
+    },
+    address_type: data.address_type,
+  });
+
+  address.save();
+  console.log("Address is created!");
+
+  return address;
+};
+
+export const createItinerary = (address, tour) => {
   const itinerary = new Itinerary({
-    tour_id: data.tour_id,
+    tour_id: tour.id,
     tour: data.tour,
-    address: data.address,
+    agency_id: tour.agency.id,
+    address: address,
   });
 
   itinerary.save();
-  console.log("Itinerary created successfully");
+  console.log("Itinerary is created!");
 
   return itinerary;
-}
+};
+
