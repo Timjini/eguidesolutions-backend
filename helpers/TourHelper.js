bcab5082b202443dbd986619a24863da4b02020a
Necmi Gunduz <necmigunduz@gmail.com>
2024-07-09 16:33:39 +0300

    fix: issues in itinerary and address creation

--- helpers/TourHelper.js
+++ helpers/TourHelper.js
@@ -1,9 +1,9 @@
+const mongoose = require('mongoose');
 const Address = require('../models/Address');
 const Itinerary = require('../models/Itinerary');
 
 class TourHelper {
   static async createAddress(data) {
-    console.log("ADDRESS DATA", data)
     const address = new Address({
       street_1: data.street_1,
       street_2: data.street_2,
@@ -19,7 +19,7 @@ class TourHelper {
     });
 
     await address.save();
-    console.log("Address is created!");
+
     return address;
   }
 
@@ -29,18 +29,32 @@ class TourHelper {
     let stops = [];
 
     for (const address of addresses) {
+      const addressObject = {
+        street_1: address.street_1,
+        street_2: address.street_2,
+        city: address.city,
+        state: address.state,
+        country: address.country,
+        postal_code: address.postal_code,
+        coordinates: {
+          lat: address.coordinates.lat,
+          lng: address.coordinates.lng,
+        },
+        address_type: address.address_type
+      };
+
       if (address.address_type === 0) {
-        start_point = { lng: address.coordinates.lng, lat: address.coordinates.lat };
+        start_point = addressObject;
       } else if (address.address_type === 1) {
-        end_point = { lng: address.coordinates.lng, lat: address.coordinates.lat };
+        end_point = addressObject;
       } else if (address.address_type === 2) {
-        stops.push({ lng: address.coordinates.lng, lat: address.coordinates.lat });
+        stops.push(addressObject);
       }
     }
 
     const itinerary = new Itinerary({
-      tour: tour.id,
-      agency: tour.agency.id,
+      tour: new mongoose.Types.ObjectId(tour.id),
+      agency: new mongoose.Types.ObjectId(tour.agency.id),
       start_point: start_point,
       end_point: end_point,
       stops: stops,
--- models/Tours.js
+++ models/Tours.js
@@ -1,4 +1,5 @@
 const mongoose = require('../db');
+const addressSchema = require('./Address').schema;
 
 const tourSchema = new mongoose.Schema({
     title: { type: String, required: true },
@@ -6,16 +7,12 @@ const tourSchema = new mongoose.Schema({
     photo: { type: String, required: false }, // You can store the photo URL here
     guide: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     agency: { type: mongoose.Schema.Types.ObjectId, ref: 'Agency', required: true },
-    starting_date: { type: Date, ref: 'Starting Date', required: false },
-    ending_date: { type: Date, ref: 'Ending Date', required: false },
-    start_point: { type: String, ref: 'Start Point', required: true},
-    end_point: { type: String, ref: 'End Point', required: true},
-    stops: { type: Array, required: false}
+    starting_date: { type: Date, required: false },
+    ending_date: { type: Date, required: false },
+    start_point: { type: addressSchema, required: true },
+    end_point: { type: addressSchema, required: true },
+    stops: { type: [addressSchema], required: false } // Array of addressSchema
 });
-
-
-// Create an index for the startLocation and endLocation fields
-// tourSchema.index({ startLocation: '2dsphere', endLocation: '2dsphere' });
 
 // Create and export the Tour model
 const Tour = mongoose.model('Tour', tourSchema);
