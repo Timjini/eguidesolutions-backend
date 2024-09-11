6478ec7ecfeb7898a51dfdd9a5336c1ad502cebb
Necmi Gunduz <necmigunduz@gmail.com>
2024-09-11 19:55:34 +0300

    fix: UserProfile name in mongoose.model

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -23,6 +23,6 @@ const userProfileSchema = new mongoose.Schema({
 });
 
 // Create the model using the correct schema name
-const UserProfile = mongoose.model('User', userProfileSchema);
+const UserProfile = mongoose.model('UserProfile', userProfileSchema);
 
 module.exports = UserProfile;
