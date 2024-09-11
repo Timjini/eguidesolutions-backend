d49324c4a7aa73c95286be5774ccdd4a73306b0a
Necmi Gunduz <necmigunduz@gmail.com>
2024-09-11 19:45:21 +0300

    add: routes for user profile and verify token

--- api/v2/userProfileRoutes.js
+++ api/v2/userProfileRoutes.js
@@ -1,1 +1,11 @@
 // router.get("/v2/user-profile", UserProfileController.getUser);
+const express = require("express");
+const router = express.Router();
+
+const UserProfileController = require('../../controllers/v2/userProfileController');
+const verifyToken = require('../../auth/authMiddleware');
+
+router.get("/", UserProfileController.getUserProfile);
+router.post("/new_user_profile", verifyToken, UserProfileController.createUserProfile);
+router.put("/update_user_profile", verifyToken, UserProfileController.updateUserProfile);
+router.delete("/delete_user_profile", verifyToken, UserProfileController.deleteUserProfile);
