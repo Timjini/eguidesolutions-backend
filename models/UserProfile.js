74dada1b7737bd19726df5fa1ebd54fe3b41352e
Necmi Gunduz <necmigunduz@gmail.com>
2024-09-11 16:14:53 +0300

    add: UserProfile model

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,22 +1,28 @@
 const mongoose = require('../db');
 
-
+// Define the schema
 const userProfileSchema = new mongoose.Schema({
-  id: {type: {String}, required: true, unique: true },
-  country: {String},
-  city: {String},
-  address: {String},
-  lng: Number,
-  lat: Number,
-  dob: Date,
-  department: {String},
-  selectedLanguage: {String},
-  timeZone: {String},
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  country: { type: String, required: false },
+  city: { type: String, required: false },
+  address: { type: String, required: false },
+  coordinates: {
+    lng: { type: Number },
+    lat: { type: Number },
+  },
+  dob: { type: Date, required: false },
+  department: { type: String, required: false },
+  selectedLanguage: { type: String, required: false },
+  timeZone: { type: String, required: false },
   createdAt: { type: Date, required: true, default: Date.now },
   updatedAt: { type: Date, required: true, default: Date.now },
 });
 
-
-const User = mongoose.model('User', userSchema);
+// Create the model using the correct schema name
+const User = mongoose.model('User', userProfileSchema);
 
 module.exports = User;
