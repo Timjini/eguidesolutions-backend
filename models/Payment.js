b9cc21152074d0cabe1d5f9038f0c50c30d3563b
Timjini <hatim.jini@gmail.com>
2024-09-15 16:09:20 +0200

    models for subscriptions, packages and payments

---
+++ models/Payment.js
@@ -0,0 +1,18 @@
+const mongoose = require('../db');
+
+const paymentSchema = new mongoose.Schema({
+    agency: { type: mongoose.Schema.Types.ObjectId, ref: 'Agency', required: true },
+    subscription: { type: mongoose.Schema.Types.ObjectId, ref: 'Subscription', required: true },
+    package: { type: mongoose.Schema.Types.ObjectId, ref: 'SubscriptionPackage', required: true },
+    amount: { type: Number, required: true },
+    paymentDate: { type: Date, default: Date.now }, 
+    status: { 
+        type: String, 
+        enum: ['pending', 'completed', 'failed'], 
+        default: 'pending' 
+    },
+    transactionId: { type: String, required: false } 
+});
+
+const Payment = mongoose.model('Payment', paymentSchema);
+module.exports = Payment;
