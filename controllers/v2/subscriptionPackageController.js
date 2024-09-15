09d75a2593cb71a398558a9b0923def4a02e744b
Timjini <hatim.jini@gmail.com>
2024-09-16 01:07:36 +0200

    package routes

--- controllers/v2/subscriptionPackageController.js
+++ controllers/v2/subscriptionPackageController.js
@@ -1,5 +1,8 @@
+const SubscriptionPackage = require("../../models/SubscriptionPackage");
+
 async function createPackage(req, res) {
   try {
+    console.log(req.body);
     const { name, description, price, duration } = req.body;
     const newPackage = new SubscriptionPackage({
       name,
@@ -10,10 +13,21 @@ async function createPackage(req, res) {
     await newPackage.save();
     res.status(201).json(newPackage);
   } catch (error) {
-    res.status(500).json({ error: "Failed to create subscription package" });
+    res.status(500).json({ error: "Failed to create subscription package", error: error.message });
+  }
+}
+
+async function getPackages(req, res) {
+  try {
+    console.log("Fetching packages");
+    const packages = await SubscriptionPackage.find();
+    res.status(200).json(packages);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch subscription packages", error: error.message });
   }
 }
 
 module.exports = {
     createPackage,
+    getPackages
   };
