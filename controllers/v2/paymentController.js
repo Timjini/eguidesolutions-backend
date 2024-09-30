7e13a25efc007a5481cdf28a986d3a0b6564d220
Timjini <hatim.jini@gmail.com>
2024-09-30 21:26:28 +0200

    payment controller

--- controllers/v2/paymentController.js
+++ controllers/v2/paymentController.js
@@ -2,49 +2,106 @@ const Agency = require("../../models/Agency");
 const Subscription = require("../../models/Subscription");
 const SubscriptionPackage = require("../../models/SubscriptionPackage");
 const Payment = require("../../models/Payment");
+const PaymentSerializer = require("../../serializers/v2/PaymentSerializer");
 
-async function createPayment(req, res) {
+async function createPayment(req, res){
     try {
-      const { agencyId, subscriptionId, packageId, amount } = req.body;
+      const { agencyId, packageId, amount } = req.body;
+
       const agency = await Agency.findById(agencyId);
-      if (!agency) {
-        return res.status(404).json({ error: "Agency not found" });
+          if (!agency) {
+            return res.status(404).json({ error: "Agency not found" });
+          }
+          const package = await SubscriptionPackage.findById(packageId);
+          if (!package) {
+            return res.status(404).json({ error: "Subscription package not found" });
+          }
+
+    const subscription = await Subscription.findOne({
+      agency: agencyId,
+    }).exec();
+
+      if(!subscription) {
+        try {
+          let today = new Date();
+          let durationToMonth = package.durationInMonths;
+          let startDate = new Date();
+          let endDate = new Date(
+            today.getTime() + durationToMonth * 24 * 60 * 60 * 1000
+          );
+          // create e a new subscription
+          try {
+            let subscription = new Subscription({
+              agency: agencyId,
+              package: packageId,
+              startDate: startDate || new Date(),
+              endDate,
+              status: "pending",
+            });
+            await subscription.save();
+          } catch (err) {
+            console.log("==================",err);
+          }
+        } catch (err) {
+          console.log("==================",err);
+        }
       }
-      const subscription = await Subscription.findById(subscriptionId);
-      if (!subscription) {
-        return res.status(404).json({ error: "Subscription not found" });
-      }
-      const package = await SubscriptionPackage.findById(packageId);
-      if (!package) {
-        return res.status(404).json({ error: "Subscription package not found" });
-      }
-      const newPayment = new Payment({
-        agency: agencyId,
-        subscription: subscriptionId,
-        package: packageId,
-        amount,
-        status: "pending",
-      });
-      await newPayment.save();
-        //TODO adapt subscriptions helper   after payment is successful, update subscription status to active
-        subscription.status = "active";
-        subscription.payment_status = "paid";
-        await subscription.save();
-      res.status(201).json(newPayment);
+
+      let newPayment;
+          try {
+            const newPayment = new Payment({
+              agency: agencyId,
+              subscription: subscription?._id,
+              package: packageId,
+              amount,
+              status: "pending",
+            });
+            await newPayment.save();
+          } catch (err) {
+            console.log("==================",err);
+          }
+
+          res.status(201).json(newPayment);
     } catch (err) {
-      res.status(500).json({ error: "Failed to create payment" });
-      }
+      console.log("==================",err);
     }
+}
 
 async function getPayments(req, res) {
   try {
+    // Retrieve all payments
     const payments = await Payment.find();
-    res.status(200).json(payments);
+
+    // Serialize all payments
+    const serializedPayments = await Promise.all(
+      payments.map((payment) => PaymentSerializer.serialize(payment))
+    );
+
+    // Send the serialized payments as the response
+    res.status(200).json(serializedPayments);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: "Failed to retrieve payments" });
   }
 }
-    module.exports = {
-      createPayment,
-      getPayments
-    };
+
+async function updatePayment(req, res) {
+  try {
+    const { id } = req.params;
+    const { status , amount , paymentDate } = req.body;
+    const payment = await Payment.findByIdAndUpdate(id, { amount, status, paymentDate }, { new: true });
+    res.status(200).json(payment);
+    if (!payment) {
+      return res.status(404).json({ error: "Payment not found" });
+    }
+  }
+    catch (err) {
+    res.status(500).json({ error: "Failed to update payment" });
+  }
+}
+
+module.exports = {
+  createPayment,
+  getPayments,
+  updatePayment,
+};
