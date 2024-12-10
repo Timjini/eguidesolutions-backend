b98767962ce4c05abe3149e4a6470bd2a782338a
Timjini <hatim.jini@gmail.com>
2024-12-10 21:30:36 +0100

    add a joblog table and a cron job to refresh channeltoken

---
+++ models/JobLog.js
@@ -0,0 +1,12 @@
+const mongoose = require('../db');
+
+const jobLogSchema = new mongoose.Schema({
+  jobName: { type: String, required: true },
+  executionTime: { type: Date, default: Date.now },
+  status: { type: String, enum: ['success', 'error'], required: true },
+  errorMessage: { type: String, default: '' },
+});
+
+const JobLog = mongoose.model('JobLog', jobLogSchema);
+
+module.exports = JobLog;
