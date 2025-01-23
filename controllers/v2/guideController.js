13145e45159cc5753c7f0bf1fd0151de59782b6b
Timjini <hatim.jini@gmail.com>
2025-01-23 22:29:03 +0100

    guide controller, serializer and new routes to get guide; new rating table and rating service with polymorphic relation to rate other tables if required

--- controllers/v2/guideController.js
+++ controllers/v2/guideController.js
@@ -1,4 +1,31 @@
 const Guide = require("../../models/Guide");
-const GuideReviews = require("../../models/GuideReviews");
+const GuideSerializer = require("../../serializers/v2/GuideSerializer");
 
+async function getGuide(req, res) {
+  try {
+    const guideId = req.body.guideId._id;
+    console.log(guideId);
+    const guide = await Guide.findOne({ _id: guideId });
+    console.log("guide----->", guide);
 
+    // Serialize each agency using the updated serializer
+    //   const serializedAgencies = await Promise.all(
+    //     agencies.map((agency) => AgencySerializer.serialize(agency))
+    //   );
+
+    const serializedGuide = await GuideSerializer.serialize(guide);
+
+    return res.status(200).json({
+      status: "success",
+      message: "Guide fetched successfully",
+      guide: serializedGuide,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: error, message: "Internal server error" });
+  }
+}
+
+module.exports = {
+  getGuide,
+};
--- serializers/v2/GuideSerializer.js
+++ serializers/v2/GuideSerializer.js
@@ -1,5 +1,7 @@
 
+const Agency = require("../../models/Agency");
 const Guide = require("../../models/Guide");
+const UserProfile = require("../../models/UserProfile");
 const User = require("../../models/Users");
 
 class GuideSerializer {
@@ -7,12 +9,27 @@ class GuideSerializer {
         if (!guide) {
         return null;
         }
-
+        const user = await User.findOne({_id: guide?.user});
+        console.log("user id ", user._id)
+        const userProfile = await UserProfile.findOne({user: user._id})
+        console.log("user", userProfile);
+        const agency = await Agency.findOne({_id: guide?.agency});
+        console.log("agency," , agency);
         return {
-            
+            _id: guide?._id,
+            name: user?.name,
+            email: user?.email,
+            phone: user?.phone,
+            avatar:user?.avatar,
+            rating: 3,
+            comments:[
+              "great tour with the guide", "had a lot of fun in this tour."
+            ],
+            agency_name: agency?.name,
+            agency_image: agency?.image
         }
     }
 }
 
 
-module.export = GuideSerializer;
+module.exports = GuideSerializer;
