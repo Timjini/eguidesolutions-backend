03522f57f9963e864f72b84192cc47cad3f0a6c8
Timjini <hatim.jini@gmail.com>
2025-02-28 22:50:11 +0100

    readme

--- api/v1/auth/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../../../controllers/authController");
-
-router.post("/login", authController.loginAuth);
-router.delete("/logout", authController.logoutAuth);
-router.post("/sign_up", authController.signUpAuth);
-
-module.exports = router;
--- api/v1/tours/touristToursRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const TourController = require("../../../controllers/tourController");
-
-router.get("/", TourController.getAllTours);
-router.post("/new_route", TourController.createNewTour);
-router.get("/agency_tours", TourController.getAgencyTours);
-
-module.exports = router;
