e3c4b568f8ebde92e67a8b01da4cc19325cfadca
Timjini <hatim.jini@gmail.com>
2025-02-28 22:50:11 +0100

    readme

--- api/v1/favorite/favoriteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const FavoriteController = require("../../../controllers/favoriteController");
-
-router.post("/", FavoriteController.addToFavorite);
-router.delete("/", FavoriteController.removeFromFavorite);
-
-module.exports = router;
--- peer.js
+++
@@ -1,5 +0,0 @@
-console.log('Initializing PeerJS Server...');
-const { PeerServer } = require('peer');
-const server = PeerServer({ port: 5000 });
-console.log('Peer server running on port 5000');
-module.exports = server;
