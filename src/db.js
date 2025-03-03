fe0f21bf2986483ec6956ea50f3ba01a219dcb4d
Timjini <hatim.jini@gmail.com>
2025-03-03 10:30:29 +0100

    roll back uri

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGODB_MAIN_URI = process.env.MONGODB_URI;
+const MONGODB_MAIN_URI = process.env.MONGODB_MAIN_URI;
 mongoose.connect(MONGODB_MAIN_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
